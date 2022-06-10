047ad0c3a6215001b49c157b05987a0acab2d305
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2022-06-10 18:57:21 +0530

    Update Algoliasearch.js

--- Components/Algoliasearch.js
+++ Components/Algoliasearch.js
@@ -5,7 +5,7 @@ const searchClient = algoliasearch(
   '915e8aaa9d44c1abc0606a5a58b7ecb1'
 );
 export default function Algoliasearch(){
-  <InstantSearch
+  return <InstantSearch
     indexName="netlify_29bacbcb-7b82-4e5d-92b8-80d58d0299d2_main_all"
     searchClient={searchClient}
   >
