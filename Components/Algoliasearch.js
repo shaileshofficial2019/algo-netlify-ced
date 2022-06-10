325fd841e7b98ec901074bf8f1c55b1104d184af
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2022-06-10 18:51:32 +0530

    Update and rename docsearch.js to Algoliasearch.js

---
+++ Components/Algoliasearch.js
@@ -0,0 +1,15 @@
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+const searchClient = algoliasearch(
+  'KJH0XYL1BE',
+  '915e8aaa9d44c1abc0606a5a58b7ecb1'
+);
+export default function Algoliasearch(){
+  <InstantSearch
+    indexName="netlify_29bacbcb-7b82-4e5d-92b8-80d58d0299d2_main_all"
+    searchClient={searchClient}
+  >
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
+}
--- Components/docsearch.js
+++
@@ -1,3 +0,0 @@
-export default function () {
-  return <div className="relative w-full md:w-64 mr-2 docs-search" id="search"></div>
-}
