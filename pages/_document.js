2db6215045de95533afdc00d71882fc37783f537
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2022-06-10 18:08:44 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,14 +18,8 @@ export default function Document() {
             {dd}
             </script>
       <body>
-        <div id="search"></div>
         <Main />
         <NextScript />
-    <script
-            src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
-            async
-            defer
-          />
       </body>
     </Html>
   )
