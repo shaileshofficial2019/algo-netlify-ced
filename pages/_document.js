828905552b704a5316aeb236d12ef201cebb8910
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2022-06-11 11:12:26 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-    const dd= `algoliasearchNetlify({
-        appId:'KJH0XYL1BE',
-        apiKey:'915e8aaa9d44c1abc0606a5a58b7ecb1',
-        siteId:'29bacbcb-7b82-4e5d-92b8-80d58d0299d2',
-        branch:'master',
-        selector:'div#search'
-    });`
+    const dd= `docsearch({
+        container: '#docsearch',
+        appId: process.env.DOC_APP_ID,
+        apiKey: process.env.DOC_API_KEY,
+        indexName: process.env.DOC_INDEX.NAME,
+        getMissingResultsUrl({ query }) {
+    return `https://github.com/shaileshofficial2019/algo-netlify-ced/issues/new?title=${query}`;
+  }
+});`
   return (
     <Html>
       <Head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@docsearch/js@3"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
       </Head>
         <script type="text/javascript">
             {dd}
